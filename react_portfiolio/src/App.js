b6c6343ad602cb0f150b4a612a414cd1bf5b9209
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-06-03 11:00:25 -0300

    prepared project to begin working on components

---
+++ react_portfiolio/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Header } from "./components/header";
+import { Navbar } from "./Components/navbar";
+import { Navigation } from "./components/navigation";
+import { Project } from "./components/Projects";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_portfiolio/src/App.test.js
@@ -0,0 +1,24 @@
+// import react and testing dependencies
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+// import component files
+import { Header } from "./components/header";
+import { Navbar } from "./Components/navbar";
+import { Navigation } from "./components/navigation";
+import { Project } from "./components/Projects";
+import App from "./App";
+
+let container = null;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
